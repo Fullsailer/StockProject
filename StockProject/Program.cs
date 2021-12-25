ebc0665dc59e41d60bb0b36ac9889de941be1076
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2021-12-24 22:00:26 -0500

    first commit

---
+++ StockProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StockProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
